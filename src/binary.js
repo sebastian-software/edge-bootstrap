389aac032d718e33cd8d469539e3a22d6aeb88f3
Sebastian Fastner <s.fastner@sebastian-software.de>
2018-02-09 16:56:53 +0100

    Check if package.json and template.tar exists

--- src/binary.js
+++ src/binary.js
@@ -1,5 +1,13 @@
 import chalk from "chalk"
+
+import fs from "fs"
+import util from "util"
+import path from "path"
+
 import pkg from "../package.json"
+
+const fsAccess = util.promisify(fs.access)
+const CWD = process.cwd()
 
 const IS_INTERACTIVE = process.stdout.isTTY
 
@@ -9,4 +17,24 @@ if (IS_INTERACTIVE) {
   )
 }
 
-console.log(chalk.bold(`edge bootstrap ${chalk.green(`v${pkg.version}`)}`))
+async function main() {
+  console.log(chalk.bold(`edge bootstrap ${chalk.green(`v${pkg.version}`)}`))
+  console.log()
+
+  try {
+    await fsAccess(path.join(CWD, "package.json"))
+  } catch ({}) {
+    console.log(chalk.red(`No package.json found in current path ${CWD}`))
+    console.log(chalk.cyan(`> You might want to run ${chalk.white.bold("npm init")} first`))
+    return
+  }
+
+  try {
+    await fsAccess(path.join(__dirname, "..", "template.tar"))
+  } catch ({}) {
+    console.log(chalk.red(`No template.tar found in path ${__dirname}/..`))
+    return
+  }
+}
+
+main().catch(error => console.log(chalk.red(error.stack)))
